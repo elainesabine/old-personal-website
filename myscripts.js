7b453ef2a31a29e337dcf9c47b2bd333fe7e0c98
Elaine Pranadjaya <elainesabine@gmail.com>
2019-03-29 01:08:44 -0700

    carousel works!

--- myscripts.js
+++ myscripts.js
@@ -3,51 +3,43 @@ $( function() {
   $( "#tabs" ).tabs();
 } );
  
-// $(document).ready(function () {
-//   $('.single-item').slick({
-//       infinite: true,
-//       slidesToShow: 1,
-//       slidesToScroll: 1,
-//       arrows: true
-//   });
-//   });
-
-
-
-
-$(function(){
-  $('.carousel-item').eq(0).addClass('active');
-  var total = $('.carousel-item').length;
-  var current = 0;
-  $('#moveRight').on('click', function(){
-    var next=current;
-    current= current+1;
-    setSlide(next, current);
-  });
-  $('#moveLeft').on('click', function(){
-    var prev=current;
-    current = current- 1;
-    setSlide(prev, current);
-  });
-  function setSlide(prev, next){
-    var slide= current;
-    if(next>total-1){
-     slide=0;
-      current=0;
-    }
-    if(next<0){
-      slide=total - 1;
-      current=total - 1;
-    }
-           $('.carousel-item').eq(prev).removeClass('active');
-           $('.carousel-item').eq(slide).addClass('active');
-      setTimeout(function(){
-
-      },800);
-    
-
-    
-    console.log('current '+current);
-    console.log('prev '+prev);
-  }
+$('.carousel').carousel({
+  interval: 10000
 });
+
+// $(function(){
+//   $('.carousel-item').eq(0).addClass('active');
+//   var total = $('.carousel-item').length;
+//   var current = 0;
+//   $('#moveRight').on('click', function(){
+//     var next=current;
+//     current= current+1;
+//     setSlide(next, current);
+//   });
+//   $('#moveLeft').on('click', function(){
+//     var prev=current;
+//     current = current- 1;
+//     setSlide(prev, current);
+//   });
+//   function setSlide(prev, next){
+//     var slide= current;
+//     if(next>total-1){
+//      slide=0;
+//       current=0;
+//     }
+//     if(next<0){
+//       slide=total - 1;
+//       current=total - 1;
+//     }
+//            $('.carousel-item').eq(prev).removeClass('active');
+//            $('.carousel-item').eq(slide).addClass('active');
+//       setTimeout(function(){
+
+//       },800);
+    
+
+    
+//     console.log('current '+current);
+//     console.log('prev '+prev);
+//   }
+// });
