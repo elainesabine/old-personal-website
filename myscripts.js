a0113917375a6c2cf6dc69fe22cf19866b3d2181
Elaine Pranadjaya <elainesabine@gmail.com>
2019-03-28 20:45:23 -0700

    projects tab

--- myscripts.js
+++ myscripts.js
@@ -1,26 +1,50 @@
+
 $( function() {
   $( "#tabs" ).tabs();
 } );
+ 
+// $(document).ready(function () {
+//   $('.single-item').slick({
+//       infinite: true,
+//       slidesToShow: 1,
+//       slidesToScroll: 1,
+//       arrows: true
+//   });
+//   });
 
-
-$('#clickme').click(function() {
-  $('#book').fadeOut('slow', function() {
-    // Animation complete.
+$(function(){
+  $('.carousel-item').eq(0).addClass('active');
+  var total = $('.carousel-item').length;
+  var current = 0;
+  $('#moveRight').on('click', function(){
+    var next=current;
+    current= current+1;
+    setSlide(next, current);
   });
+  $('#moveLeft').on('click', function(){
+    var prev=current;
+    current = current- 1;
+    setSlide(prev, current);
+  });
+  function setSlide(prev, next){
+    var slide= current;
+    if(next>total-1){
+     slide=0;
+      current=0;
+    }
+    if(next<0){
+      slide=total - 1;
+      current=total - 1;
+    }
+           $('.carousel-item').eq(prev).removeClass('active');
+           $('.carousel-item').eq(slide).addClass('active');
+      setTimeout(function(){
+
+      },800);
+    
+
+    
+    console.log('current '+current);
+    console.log('prev '+prev);
+  }
 });
-
-$(document).ready(function(){
-  initTabs();
-});
-
-function initTabs() {
-  $('#tabMenu a').bind('click',function(e) {
-  e.preventDefault();
-  var thref = $(this).attr("href").replace(/#/, '');
-  $('#tabMenu a').removeClass('active');
-  $(this).addClass('active');
-  $('#tabContent div.content[id!='+thref+']').fadeOut('slow', function(){
-    $('#'+thref).fadeIn('slow');
-  });
-  });
-}
